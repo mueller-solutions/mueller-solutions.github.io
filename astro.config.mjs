9d9800fc21d76ab68d93252cfaed6d89f1e421ab
Florian Müller <flomueller1981@gmail.com>
2024-06-22 16:54:10 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mueller-solutions.github.io",
+  site: "https://mueller-solutions.dev",
   integrations: [tailwind()],
 });
