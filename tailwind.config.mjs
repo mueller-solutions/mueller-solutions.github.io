d5ea1c5fe055fa8ac78ad12a15ec49853254c1f1
Florian Müller <flomueller1981@gmail.com>
2024-06-26 21:23:32 +0200

    Homepage first setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,14 @@ export default {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        section: "0px 0px 6px 3px rgb(0 0 0 / 25%)",
+        input: "inset 0px 2px 4px rgb(0 0 0 / 15%)",
+        "input-hover": "inset 0px 3px 6px rgb(0 0 0 / 25%)",
+      },
+      backgroundImage: {
+        hero: "#0B59AE, url('/images/hero-bg.png')",
+      },
     },
   },
   plugins: [],
