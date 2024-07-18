a5043a0b505803ac2f37b9fb308f127f8c894fa6
Florian Müller <flomueller1981@gmail.com>
2024-07-18 15:23:09 +0200

    Add build optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import purgecss from "astro-purgecss";
+import playformCompress from "@playform/compress";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mueller-solutions.dev",
-  integrations: [purgecss()]
+  integrations: [purgecss(), sitemap(), robotsTxt(), playformCompress()],
+  build: {
+    inlineStylesheets: "always",
+  },
 });
