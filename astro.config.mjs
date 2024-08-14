1469776b7ed4f18688cb66200e8c6c24a5b2a049
Florian Müller <flomueller1981@gmail.com>
2024-08-14 14:26:34 +0200

    Optimize contents and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,30 @@ import playformCompress from "@playform/compress";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
+const siteURL = "https://mueller-solutions.dev";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mueller-solutions.dev",
-  integrations: [purgecss(), sitemap(), robotsTxt(), playformCompress()],
+  site: siteURL,
+  integrations: [
+    purgecss(),
+    sitemap({
+      filter: (page) => page !== `${siteURL}/booking-confirmed/`,
+    }),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          disallow: ["/privacy-policy"],
+        },
+        {
+          userAgent: "*",
+          disallow: ["/booking-confirmed"],
+        },
+      ],
+    }),
+    playformCompress(),
+  ],
   build: {
     inlineStylesheets: "always",
   },
