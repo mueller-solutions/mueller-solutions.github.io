429664056c52bb81aa6c44529297abc8287c9298
Florian Müller <flomueller1981@gmail.com>
2024-10-14 18:52:04 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-// import playformCompress from '@playform/compress';
-// import robotsTxt from 'astro-robots-txt';
-// import sitemap from '@astrojs/sitemap';
+import playformCompress from '@playform/compress';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 const siteURL = 'https://mueller-solutions.dev';
 
@@ -11,24 +11,24 @@ export default defineConfig({
   site: siteURL,
   output: 'server',
   adapter: netlify(),
-  // integrations: [
-  //   sitemap({
-  //     filter: (page) => page !== `${siteURL}/booking-confirmed/`,
-  //   }),
-  //   robotsTxt({
-  //     policy: [
-  //       {
-  //         userAgent: '*',
-  //         disallow: ['/privacy-policy'],
-  //       },
-  //       {
-  //         userAgent: '*',
-  //         disallow: ['/booking-confirmed'],
-  //       },
-  //     ],
-  //   }),
-  //   playformCompress(),
-  // ],
+  integrations: [
+    sitemap({
+      filter: (page) => page !== `${siteURL}/booking-confirmed/`,
+    }),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          disallow: ['/privacy-policy'],
+        },
+        {
+          userAgent: '*',
+          disallow: ['/booking-confirmed'],
+        },
+      ],
+    }),
+    playformCompress(),
+  ],
   build: {
     inlineStylesheets: 'always',
     format: 'file',
