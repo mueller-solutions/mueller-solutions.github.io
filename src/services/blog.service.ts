7164dd0be7daed77a322641dba1664f5c8feb4eb
Florian Müller <flomueller1981@gmail.com>
2024-11-06 12:10:31 +0100

    [MAR-21] fix blog and blog-posts

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -6,14 +6,24 @@ class BlogService {
   }
 
   async getPosts(page: number = 1) {
-    const response = await this.client.get(`posts?_embed&per_page=20&page=${page}&orderby=date&order=desc`);
-    const numPages = parseInt(response.headers['x-wp-totalpages'], 10);
-    return { posts: response.data, numPages, currentPage: page };
+    try {
+      const response = await this.client.get(`posts?_embed&per_page=20&page=${page}&orderby=date&order=desc`);
+      const numPages = parseInt(response.headers['x-wp-totalpages'], 10);
+      return { posts: response.data, numPages, currentPage: page };
+    } catch (error) {
+      console.error(error);
+      return { posts: [], numPages: 0, currentPage: page };
+    }
   }
 
   async getPost(slug: string) {
-    const response = await this.client.get(`posts?slug=${slug}&_embed`);
-    return response.data;
+    try {
+      const response = await this.client.get(`posts?slug=${slug}&_embed`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   }
 }
 
